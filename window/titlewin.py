18c66f494dd93a231c83bf9cc9f5178d9c30d876
gluttony3 <kaminskijbogdan39@gmail.com>
2025-08-06 15:37:04 +0300

    Add files via upload

---
+++ window/titlewin.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui import Ui_MainWindow
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
